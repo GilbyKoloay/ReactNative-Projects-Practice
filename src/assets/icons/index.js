58105f0857154db2a19d413fdd968138af3e3287
GilbyKoloay <lolwtf10001@gmail.com>
2022-12-27 11:54:14 +0800

    add assets (fonts, images, and icons)

---
+++ src/assets/icons/index.js
@@ -0,0 +1,23 @@
+// icons
+
+import ArrowLeft from './ArrowLeft.svg';
+import Boxes from '/Boxes.svg';
+import Cart from './Cart.svg';
+import Chair from './Chair.svg';
+import Eye from './Eye.svg';
+import Home from './Home.svg';
+import Person from './Person.svg';
+import Pinpoint from './Pinpoint.svg';
+import Ticket from './Ticket.svg';
+
+export {
+  ArrowLeft,
+  Boxes,
+  Cart,
+  Chair,
+  Eye,
+  Home,
+  Person,
+  Pinpoint,
+  Ticket,
+};
---
+++ src/assets/images/index.js
@@ -0,0 +1,19 @@
+// images
+
+import Camera from './Camera.png';
+
+// movies
+import Avatar from './movies/Avatar.png';
+import AvatarWp from './movies/Avatar Wp.png';
+import BlackAdam from './movies/Black Adam.png';
+import WakandaForever from './movies/Wakanda Forever.png';
+
+export {
+    Camera,
+
+    // movies
+    Avatar,
+    AvatarWp,
+    BlackAdam,
+    WakandaForever,
+};
