8144b474bee1a49a148fdb09b4e249afe816f7a9
Gilby Koloay <lolwtf10001@gmail.com>
2023-05-08 22:50:36 +0800

    added images and vectors

---
+++ src/images/index.js
@@ -0,0 +1,17 @@
+// images
+
+import Gallon from './gallon.png';
+import Gopay from './gopay.png';
+import LPG from './lpg.png';
+import OVO from './ovo.png';
+import TitleBG from './titleBG.png';
+import TitleOAS from './titleOAS.png';
+
+export {
+  Gallon,
+  Gopay,
+  LPG,
+  OVO,
+  TitleBG,
+  TitleOAS
+};
---
+++ src/vectors/index.js
@@ -0,0 +1,35 @@
+// vectors
+
+import Account from './account.svg';
+import ArrowLeft from './arrowLeft.svg';
+import Card from './card.svg';
+import Cart from './cart.svg';
+import Decrease from './decrease.svg';
+import Explore from './explore.svg';
+import Filter from './filter.svg';
+import Home from './home.svg';
+import HomeLight from './homeLight.svg';
+import Increase from './increase.svg';
+import Lock from './lock.svg';
+import Pencil from './pencil.svg';
+import Search from './search.svg';
+import Star from './star.svg';
+import UpDownArrow from './upDownArrow.svg';
+
+export {
+  Account,
+  ArrowLeft,
+  Card,
+  Cart,
+  Decrease,
+  Explore,
+  Filter,
+  Home,
+  HomeLight,
+  Increase,
+  Lock,
+  Pencil,
+  Search,
+  Star,
+  UpDownArrow
+};
