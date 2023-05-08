c2702a8032a627efbb0e60024bf7fa78e8e7c832
Gilby Koloay <lolwtf10001@gmail.com>
2023-05-08 23:24:26 +0800

    setted up screens, components, and styles files

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,31 @@
-export default function App() {}
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// screens
+import {
+  Login,
+  Home,
+  Explore,
+  Account,
+  Cart
+} from './screens';
+
+
+
+const Stack = createNativeStackNavigator();
+
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown: false}}>
+        <Stack.Screen name='Login' component={Login} />
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Explore' component={Explore} />
+        <Stack.Screen name='Account' component={Account} />
+        <Stack.Screen name='Cart' component={Cart} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+// global components
+
+import Button from './Button';
+import Search from './Search';
+import Navbar from './NavBar';
+import BackButton from './BackButton';
+
+export {
+  Button,
+  Search,
+  Navbar,
+  BackButton
+};
